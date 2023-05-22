d73d9af3481b7b786668d3dc15b182cf176f59c6
soma0148 <somapauldk@gmail.com>
2023-05-22 12:57:37 +0200

    java script added

--- js/index.js
+++ js/index.js
@@ -1,20 +1,33 @@
 // Lav variablen "btn", der henviser til ".toggle-btn"
 // Lav variablen "menu", der henviser til ".main-menu"
 
+let btn = document.querySelector(".toggle-btn");
+let menu = document.querySelector(".main-menu");
+
 // Lav funktionen "toggleMenu()" med følgende funktionalitet
+
+function toggleMenu() {
+  
+
 
   // a) toggle klassen "shown" på menu vha. classList.toggle
 
+  menu.classList.toggle("shown");
+
   // b) lav variablen "menuShown", som siger, at menu-variablen indeholder klassen "shown" via classList.contains("")
 
+  let menuShown = menu.classList.contains("shown");
   // c) spørg om "menuShown" i if-sætningen nedenfor (=> if (menuShown)), og udskift teksten
-  if (...) {
+  if (menuShown) {
     console.log(menuShown); // se i konsollen
     // sæt btn.textContent til "Luk", hvis menuShown er "true"
   } else {
     console.log(menuShown); // se i konsollen
     // sæt btn.textContent til "Menu", hvis menuShown er "false"
+    btn.textContent = "Menu";
   }
 // "toggleMenu()" slutter her
+}
 
 // Tilføj et klik-event til "btn", der sætter toggleMenu-funktionen i gang
+btn.addEventListener("click", toggleMenu);
